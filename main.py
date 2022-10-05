c51b484cad1342f6e611c7a8ca76a342b510934d
nicarp96 <leoyotube0@gmail.com>
2022-10-05 18:08:26 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+necunoscuta = "n"
+utilizator = input("Ghiciti litera: ")
+while necunoscuta != utilizator :
+    print("Nu ai ghicit!")
+    utilizator = input("Ghiciti litera: ")
+    if necunoscuta == utilizator :
+        print("Ai ghicit!")
+        break;
